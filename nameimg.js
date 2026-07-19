a4ec42a0f91a6c6ddbc0f76c6874ac60987395e0
Virya Dharmawan <viryadharmawan@gmail.com>
2026-07-19 16:15:57 +0700

    Add Missing Character Banner Images

--- nameimg.js
+++ nameimg.js
@@ -28,3 +28,17 @@ await Promise.all(fs.readdirSync("./src/assets/weapons").map(async x=> {
     }
   })
 }))
+
+await Promise.all(fs.readdirSync("./src/assets/banners").map(async x=> {
+  if(x.endsWith(".webp")) return
+  await sharp("./src/assets/banners/"+x)
+      .resize(1280)
+      .webp({ quality: 80 })
+      .toFile("./src/assets/banners/"+x.split(".")[0]+".webp", (err, info) => {
+    if (err) console.error(err)
+    else {
+      console.log("Optimized", x, info)
+      fs.rmSync("./src/assets/banners/"+x)
+    }
+  })
+}))
